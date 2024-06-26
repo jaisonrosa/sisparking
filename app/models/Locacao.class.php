062e7de9400267de87db596dbb4bc27bb498334f
jaisonrosa <jaisonrosa.info@gmail.com>
2024-06-26 18:32:00 -0300

    correção class locação

--- app/models/Locacao.class.php
+++ app/models/Locacao.class.php
@@ -155,51 +155,6 @@ class Locacao {
         }
     }
 
-    /**
-     * <b>Atualizar o veiculo:</b> Envelope os dados em uma array atribuitivo e informe o id de um veiculo
-     * para atualiza-la no banco de dados!
-     * @param INT $id = id do veiculo
-     * @param ARRAY $dados = Atribuitivo
-     */
-//    public function editarVeiculo($id, array $dados) {
-//
-//        if (array_key_exists("modelo", $dados)) {
-//            $dados["modelo"] = Check::retirarAcentos(ucwords(strtolower($dados["modelo"])));
-//        }
-//        
-//        $dados["id"] = (int) Filter::toNumeric($id);
-//
-//        if (in_array('', $dados)) {
-//            $this->error = ["Erro ao Atualizar: Para atualizar este registro preencha todos os campos!"];
-//            $this->result = false;
-//        } else {
-//            $update = new Update();
-//            $update->exeUpdate($this->tabela, $dados, "WHERE id = :id", "id={$dados["id"]}");
-//
-//            if ($update->getRowCount() >= 1) {
-//                $this->result = $update->getResult();
-//            }
-//        }
-//    }
-
-    /**
-     * <b>exeDelete:</b> Deletar uma linha no banco
-     * @param INT $id = id do item no banco
-     */
-//    public function deleteVeiculo($id) {
-//        $idDelete = (int) Filter::toNumeric($id);
-//        
-//        $delete = new Delete();
-//        $delete->exeDelete($this->tabela, "WHERE id = :id", "id={$idDelete}");
-//        
-//        $this->result = $delete->getResult();
-//        if ($this->result) {
-//            $this->mensagem = "O registro foi removido com sucesso!";
-//        } else {
-//            $this->mensagem = "Epa! Confere ai deu Bug ao deletar!";
-//        }
-//    }
-//
     public function getResult() {
         return $this->result;
     }
